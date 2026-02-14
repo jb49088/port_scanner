95039aa4a1698981ee19286ea2df8bd31193192c
Justin Braun <justinbraun335@gmail.com>
2026-02-13 21:51:13 -0500

    feat: implement basic port scanning functionality

--- port_scanner.py
+++ port_scanner.py
@@ -42,6 +42,13 @@ def port_scanner():
         print("\nHost unreachable.\n")
         return
 
+    for port in range(1, 1025):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.01)
+        result = sock.connect_ex((address, port))
+        if result == 0:
+            print(f"Port {port} open.")
+
 
 if __name__ == "__main__":
     port_scanner()
